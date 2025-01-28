a96db2fd203968077f90c8cd5575f1839fb182ad
roycalvo <roy.calvo@sitevibes.com>
2025-01-27 21:14:42 -0600

    remove caching logic

--- core/components/sitevibes/index.ts
+++ core/components/sitevibes/index.ts
@@ -1,34 +1,21 @@
 const SV_API_HOST = process.env.SV_API_HOST;
 const SV_PIXEL_KEY = process.env.SV_PIXEL_KEY;
 
-const storage: any = {};
-
 export const createSiteVibesApp = async () => {
-  let result = null;
+  const url = `https://${SV_API_HOST}/pixel/${SV_PIXEL_KEY}/manager?v=1.0.2`;
+  const rs = await fetch(url, {
+    method: 'get',
+    headers: { 'Content-Type': 'application/json', Accept: 'application/json' },
+  });
+  const rsData = await rs.json();
+  const { config } = rsData;
+  const configClearText = decryptResponse(config);
 
-  const cachedRs = getConfig('SV_PIXEL_CONFIG');
-  if (cachedRs) {
-    console.log('### USING CACHED CONFIG ###');
-    result = cachedRs;
-  } else {
-    console.log('### FETCH DATA FROM SERVER ###');
-    const url = `https://${SV_API_HOST}/pixel/${SV_PIXEL_KEY}/manager?v=1.0.2`;
-    const rs = await fetch(url, {
-      method: 'get',
-      headers: { 'Content-Type': 'application/json', Accept: 'application/json' },
-    });
-    const rsData = await rs.json();
-    const { config } = rsData;
-    const configClearText = decryptResponse(config);
-
-    var configObj = eval('(function() { return ' + configClearText + '}())');
-    setConfig('SV_PIXEL_CONFIG', configObj);
-    result = configObj;
-  }
+  const configData = eval('(function() { return ' + configClearText + '}())');
 
   return {
     config() {
-      return result;
+      return configData;
     },
   };
 };
@@ -52,25 +39,4 @@ function decryptResponse(input: string): any {
   }
 
   return output;
-}
-
-function setConfig(key: string, value: any) {
-  const d = {
-    data: JSON.stringify(value),
-    expireTime: Date.now() + 60 * 1000,
-  };
-  storage[key] = JSON.stringify(d);
-}
-
-function getConfig(key: string) {
-  const data = storage[key];
-  if (!data) {
-    return null;
-  }
-  let result = JSON.parse(data);
-  if (result.expireTime <= Date.now()) {
-    delete storage[key];
-    return null;
-  }
-  return JSON.parse(result.data);
 }
